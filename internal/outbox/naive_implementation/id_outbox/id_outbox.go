68116283ba0c9c91d85e5b441df52838eceacb13
lazylex <lazylex@mail.ru>
2024-07-19 20:38:27 +0300

    Сделана наивная реализация для outbox'ов

---
+++ internal/outbox/naive_implementation/id_outbox/id_outbox.go
@@ -0,0 +1,58 @@
+/*
+Package id_outbox: наивная реализация интерфейса "github.com/lazylex/messaggio/internal/ports/id_outbox"
+*/
+
+package id_outbox
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"log/slog"
+	"sync"
+)
+
+const initSize = 10
+
+type Naive struct {
+	mu   sync.Mutex
+	ids  []uuid.UUID
+	name string
+}
+
+// New возвращает структуру для работы с outbox'ом. Для идентификации в логах используется name.
+func New(name string) *Naive {
+	return &Naive{name: name, ids: make([]uuid.UUID, 0, initSize)}
+}
+
+// Add добавление записи в outbox.
+func (n *Naive) Add(uuid uuid.UUID) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	n.ids = append(n.ids, uuid)
+
+	slog.Info(
+		fmt.Sprintf("Added to outbox: %s", uuid.String()),
+		slog.String("name", n.name),
+	)
+
+	return nil
+}
+
+// Pop извлечение записи из outbox'а.
+func (n *Naive) Pop() uuid.UUID {
+	result := uuid.Nil
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	if len(n.ids) == 0 {
+		return result
+	}
+
+	result, n.ids = n.ids[len(n.ids)-1], n.ids[:len(n.ids)-1]
+
+	slog.Info(
+		fmt.Sprintf("Pop from outbox: %s. Remaining length: %d", result.String(), len(n.ids)),
+		slog.String("name", n.name),
+	)
+
+	return result
+}
---
+++ internal/outbox/naive_implementation/record_outbox/record_outbox.go
@@ -0,0 +1,58 @@
+/*
+Package id_outbox: наивная реализация интерфейса "github.com/lazylex/messaggio/internal/ports/record_outbox"
+*/
+
+package record_outbox
+
+import (
+	"fmt"
+	"github.com/lazylex/messaggio/internal/dto"
+	"log/slog"
+	"sync"
+)
+
+const initSize = 10
+
+type Naive struct {
+	mu   sync.Mutex
+	data []dto.MessageID
+	name string
+}
+
+// New возвращает структуру для работы с outbox'ом. Для идентификации в логах используется name.
+func New(name string) *Naive {
+	return &Naive{name: name, data: make([]dto.MessageID, 0, initSize)}
+}
+
+// Add добавление записи в outbox.
+func (n *Naive) Add(data dto.MessageID) error {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	n.data = append(n.data, data)
+
+	slog.Info(
+		fmt.Sprintf("Added to outbox: %s, %s", data.ID, data.Message),
+		slog.String("name", n.name),
+	)
+
+	return nil
+}
+
+// Pop извлечение записи из outbox'а.
+func (n *Naive) Pop() dto.MessageID {
+	result := dto.MessageID{}
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	if len(n.data) == 0 {
+		return result
+	}
+
+	result, n.data = n.data[len(n.data)-1], n.data[:len(n.data)-1]
+
+	slog.Info(
+		fmt.Sprintf("Pop from outbox: %s, %s. Remaining length: %d", result.ID, result.Message, len(n.data)),
+		slog.String("name", n.name),
+	)
+
+	return result
+}
